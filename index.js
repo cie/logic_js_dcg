c69cbdbf055ad19af33265b6ed00e679cf93f013
Bernát Kalló <kallo.bernat@gmail.com>
2021-04-18 19:43:06 +0200

    implement basic dcg functions

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-function nt() {
+const { or, and, appendo, lvar, eq } = require('logic_js')
 
+function phrase(s) {
+  if (Array.isArray(s)) return (l, l1) => appendo(s, l1, l)
+  if (typeof s === 'function') {
+    if (s.length == 1) return (l, l1) => and(s, eq(l, l1))
+    if (s.length == 2) return s
+  }
+  throw new Error('Invalid argument for phrase: ' + s)
 }
-module.exports = { nt }
+
+function or_(...symbols) {
+  return (l, l1) => or(...symbols.map((b, i) => {
+    if (i === 0 && typeof b === 'number') return b // count
+    return phrase(b)(l, l1)
+  }))
+}
+
+function and_(...symbols) {
+  return (l, l1) => {
+    let cur = l
+    return and(...symbols.map((b, i) => {
+      next = i === symbols.length - 1 ? l1 : lvar()
+      const r = phrase(b)(cur, next)
+      cur = next
+      return r
+    }))
+  }
+}
+
+module.exports = { or_, and_, phrase }
--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 const { describe, it } = require('tinymocha')
-const { nt, or_, and_ } = require('.')
-const { run, lvar, eq, and, or, appendo } = require('logic_js')
+const { or_, and_, phrase } = require('.')
+const { run, lvar, eq, and, or, appendo, membero, succeed, fail } = require('logic_js')
 const assert = require('assert').strict
+
+const xit = () => {}
+
+function check(vars, logic, expected) {
+  assert.deepEqual(run(vars, logic), expected)
+}
 
 describe('logic.js-dcg', () => {
   it('can be substituted with pure logic.js', () => {
@@ -13,17 +19,104 @@ describe('logic.js-dcg', () => {
       or(eq(x, 'cat'), eq(x, 'mouse')),
       appendo([x], l1, l)
     )
-    const sentence = (x, l, l2, l1 = lvar()) => and(
+    const NP = (x, l, l2, l1 = lvar()) => and(
       determinant(l, l1),
       noun(x, l1, l2)
     )
     const x = lvar('x'), rest = lvar('rest')
-    assert.deepEqual(
-      run([x, rest], sentence(x, ['a', 'cat'], rest)),
-      [{x: 'cat', rest: []}])
+    check(
+      [x, rest],
+      NP(x, ['a', 'cat'], rest),
+      [{x: 'cat', rest: []}]
+    )
     const list = lvar('list')
-    assert.deepEqual(
-      run([x, list], sentence(x, list, [])),
+    check(
+      [x, list],
+      NP(x, list, []),
+      [
+        {x: 'cat', list: ['a', 'cat']},
+        {x: 'mouse', list: ['a', 'mouse']},
+        {x: 'cat', list: ['the', 'cat']},
+        {x: 'mouse', list: ['the', 'mouse']},
+      ]
+    )
+  })
+  describe('phrase', () => {
+    it('can parse constant terminals', () => {
+      const the = phrase(['in', 'the'])
+      check([], the(['in', 'the'], []), [{}])
+      check([], the(['in'], []), [])
+      check([], the([], []), [])
+      check([], the(['a'], []), [])
+    })
+    it('is idempotent', () => {
+      const the = phrase(phrase(['the']))
+      check([], the(['the'], []), [{}])
+      check([], the([], []), [])
+      check([], the(['a'], []), [])
+    })
+    it('can take a logic value', () => {
+      const empty = phrase(succeed())
+      const never = phrase(fail())
+      const x = lvar('x')
+      check([x], empty(['the'], x), [{x: ['the']}])
+      check([x], never(['the'], x), [])
+    })
+  })
+  describe('and_', () => {
+    it('can take a single terminal', () => {
+      const determinant = and_(['a'])
+      const x = lvar('x')
+      check([x], determinant(x, []), [{x: ['a']}])
+    })
+    it('can take terminals', () => {
+      const NP = and_(['a'], ['dog'])
+      const x = lvar('x')
+      check([x], NP(x, []), [{x: ['a', 'dog']}])
+    })
+    it('can take logic values and phrases', () => {
+      const noun = x => membero(x, ['dog', 'cat'])
+      const N = (t = lvar()) => and_(noun(t), [t])
+      const NP = and_(['a'], N())
+      const x = lvar('x')
+      check([x], NP(x, []), [{x: ['a', 'dog']}, {x: ['a', 'cat']}])
+    })
+  })
+  describe('or_', () => {
+    it('can take terminals', () => {
+      const determinant = or_(['a'], ['the'])
+      const x = lvar('x')
+      check([x], determinant(x, []), [{x: ['a']}, {x: ['the']}])
+    })
+    it('can take more terminals', () => {
+      const NP = or_(['a', 'cat'], ['the', 'mouse'])
+      const x = lvar('x')
+      check([x], NP(x, []), [{x: ['a', 'cat']}, {x: ['the', 'mouse']}])
+    })
+    it('can take phrases', () => {
+      const NP = or_(and_(['a'], ['cat']), and_(['the'], ['mouse']))
+      const x = lvar('x')
+      check([x], NP(x, []), [{x: ['a', 'cat']}, {x: ['the', 'mouse']}])
+    })
+  })
+  it('can create simple grammar', () => {
+    const determinant = or_(['a'], ['the'])
+    const noun = (x) => and_(membero(x, ['cat', 'mouse']), [x])
+    const NP = (x) => and_(determinant, noun(x))
+
+    const x = lvar('x'), rest = lvar('rest'), list = lvar('list')
+    check([rest], determinant(['a', 'cat'], rest),
+      [{rest: ['cat']}])
+    check([x, list], noun(x)(list, []),
+      [
+        {x: 'cat', list: ['cat']},
+        {x: 'mouse', list: ['mouse']}
+      ])
+    check([x, rest], noun(x)(['cat'], rest),
+      [{x: 'cat', rest: []}])
+    check([x, rest], NP(x)(['a', 'cat'], rest),
+      [{x: 'cat', rest: []}])
+    check([x, list], NP(x)(list, []),
       [
         {x: 'cat', list: ['a', 'cat']},
         {x: 'mouse', list: ['a', 'mouse']},
